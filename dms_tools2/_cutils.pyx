2a12fb9a6cbed794c4f2198cf66510ae9a02b245
jbloom <jbloom@fredhutch.org>
2017-08-04 19:35:48 -0700

    char* implementation of `buildReadConsensus`

--- dms_tools2/_cutils.pyx
+++ dms_tools2/_cutils.pyx
@@ -19,8 +19,9 @@ cimport numpy
 import dms_tools2
 
 
-# define str such that _NTS[i] == dms_tools2.NTS[i]
-cdef str _NTS = ''.join(dms_tools2.NTS)
+# define char * such that _NTS[i] == dms_tools2.NTS[i]
+cdef bytes _NTS_bytes = ''.join(dms_tools2.NTS).encode('UTF-8')
+cdef char* _NTS = _NTS_bytes
 
 
 def buildReadConsensus(list reads, int minreads, float minconcur):
@@ -34,24 +35,29 @@ def buildReadConsensus(list reads, int minreads, float minconcur):
         int maxlen = max(map(len, reads))
         int n_nts = len(dms_tools2.NTS)
         str r
-        cdef numpy.ndarray[numpy.int_t, ndim=2] counts = numpy.zeros(
+        bytes r_bytes
+        char *r_char
+        numpy.ndarray[numpy.int_t, ndim=2] counts = numpy.zeros(
                 [maxlen, n_nts], dtype=numpy.int)
 
     for r in reads:
+        r_bytes = r.encode('UTF-8')
+        r_char = r_bytes
         for i in range(len(r)):
-            if r[i] != 'N':
+            if r_char[i] != 'N':
                 for j in range(n_nts):
-                    if r[i] == _NTS[j]:
+                    if r_char[i] == _NTS[j]:
                         counts[i][j] += 1
                         break
                 else:
-                    raise ValueError("Invalid nucleotide {0}".format(r[i]))
+                    raise ValueError("Invalid nucleotide {0}".format(
+                            chr(r_char[i])))
     cdef:
         numpy.ndarray[numpy.int_t, ndim=1] ntot = counts.sum(axis=1)
         numpy.ndarray[numpy.int_t, ndim=1] xmax = counts.argmax(axis=1)
-    consensus = numpy.full(shape=maxlen, fill_value=_NTS[0])
+    consensus = numpy.full(shape=maxlen, fill_value=chr(_NTS[0]))
     for i in range(1, n_nts):
-        numpy.place(consensus, xmax == i, _NTS[i])
+        numpy.place(consensus, xmax == i, chr(_NTS[i]))
     with numpy.errstate(divide='ignore', invalid='ignore'):
         consensus[((ntot < minreads) |  ((counts.max(axis=1).astype('float') /
                 ntot) < minconcur))] = 'N'
