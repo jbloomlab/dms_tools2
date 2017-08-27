7b7ef8bed076400c7abc11022092c7113c8cab19
jbloom <jbloom@fredhutch.org>
2017-08-27 16:00:04 -0700

    more tweaks to tests

--- tests/test_pystan_compile.py
+++ tests/test_pystan_compile.py
@@ -12,13 +12,13 @@ class test_pyStanModels(unittest.TestCase):
         """Tests compilation of models."""
 
         m = dms_tools2.prefs.StanModelNoneErr(verbose=True)
-        self.asserTrue(m is not None)
+        self.assertTrue(m is not None)
     
         m = dms_tools2.prefs.StanModelSameErr(verbose=True)
-        self.asserTrue(m is not None)
+        self.assertTrue(m is not None)
 
         m = dms_tools2.prefs.StanModelDifferentErr(verbose=True)
-        self.asserTrue(m is not None)
+        self.assertTrue(m is not None)
 
 
 if __name__ == '__main__':
