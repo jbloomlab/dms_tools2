6d77a76746e6e548b6f7b716646bf38811f93972
jbloom <jbloom@fredhutch.org>
2017-09-07 13:35:48 -0700

    skeleton of `dms2_batch_diffsel`

--- setup.py
+++ setup.py
@@ -72,6 +72,7 @@ setup(
             'scripts/dms2_prefs',
             'scripts/dms2_batch_prefs',
             'scripts/dms2_diffsel',
+            'scripts/dms2_batch_diffsel',
             'scripts/dms2_logoplot',
             ],
     ext_modules = [
