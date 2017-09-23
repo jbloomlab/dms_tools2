a3ed4972ed330065b59ba6b04dc4789afc25cd37
jbloom <jbloom@fredhutch.org>
2017-09-23 11:11:15 -0700

    added `sra.fastqFromSRA`, enables aspera

---
+++ dms_tools2/sra.py
@@ -0,0 +1,144 @@
+"""
+===================
+sra
+===================
+
+Functions for downloading / handling data from the 
+`Sequence Read Archive <https://www.ncbi.nlm.nih.gov/sra>`_ (SRA).
+"""
+
+
+import os
+import shutil
+import subprocess
+import distutils.version
+
+
+def fastqFromSRA(samples, fastq_dump, fastqdir, aspera=None,
+        overwrite=False, passonly=True):
+    """Download data from SRA and extract FASTQ files.
+
+    Currently only works for runs containing paired-end reads.
+
+    Args:
+        `samples` (`pandas.DataFrame`)
+            A dataframe that must have columns named `run`
+            and `name`. The `run` column gives SRA run accessions
+            (e.g., `SRR5241726`), and the `name` column gives the
+            name for the run used in the final FASTQ files.
+        `fastq_dump` (str)
+            Path to ``fastq-dump`` executable. Requires a
+            version >= 2.8.
+        `fastqdir` (str)
+            Directory in which to place the FASTQ files.
+            Created if it does not already exist.
+        `aspera` (`None` or 2-tuple)
+            If `None`, use ``fastq-dump`` for downloads (this is slower).
+            However, downloads are faster with 
+            `aspera <https://www.ncbi.nlm.nih.gov/books/NBK242625/>`_
+            To use aspera, specify the 2-tuple `(ascp, asperakey)` where 
+            `ascp` is path to ``ascp`` executable, and `asperakey` is the
+            `key <https://www.ncbi.nlm.nih.gov/sra/docs/aspera-key-pairs/>`_.
+        `overwrite` (bool)
+            If file already exists, do we overwrite it or just
+            use the existing one?
+        `passonly` (bool)
+            Keep only reads with a passing `READ_FILTER` value.
+
+    Result:
+        Upon completion, the directory `fastqdir` contains
+        files of the form ``<name>_R1.fastq.gz`` and
+        ``<name>_R2.fastq.gz`` for all names in `samples`.
+    """
+    assert {'run', 'name'} <= set(samples.columns), \
+            '`samples` does not have columns `run` and `name`'
+
+    assert shutil.which(fastq_dump), ("fastq-dump not installed in a "
+            "location accessible with command {0}".format(fastq_dump))
+    fastq_dump_version = (subprocess.check_output([fastq_dump, '--version'])
+            .decode('utf-8').split(':')[1].strip())
+    fastq_dump_minversion = '2.8'
+    if not (distutils.version.StrictVersion(fastq_dump_version) >=
+            distutils.version.StrictVersion(fastq_dump_minversion)):
+        raise RuntimeError("fastq-dump version {0} is installed. You need "
+            "at least version {1}".format(fastq_dump_version, 
+            fastq_dump_minversion))
+
+    if aspera is not None:
+        (ascp, asperakey) = aspera
+        assert shutil.which(ascp), "ascp is not installed at {0}".format(ascp)
+        assert os.path.isfile(asperakey), "no aspera key {0}".format(asperakey)
+
+    if not os.path.isdir(fastqdir):
+        os.mkdir(fastqdir)
+
+    for row in samples.iterrows():
+        (run, name) = (row[1]['run'], row[1]['name'])
+        r1 = os.path.join(fastqdir, '{0}_R1.fastq.gz'.format(name))
+        r2 = os.path.join(fastqdir, '{0}_R2.fastq.gz'.format(name))
+
+        if (not overwrite) and os.path.isfile(r1) and os.path.isfile(r2):
+            continue # files already exist
+
+        else:
+            # names of files produced by fastq-dump
+            r1init = os.path.join(fastqdir, '{0}{1}_1.fastq.gz'.format(
+                    run, {True:'_pass', False:''}[bool(passonly)]))
+            r2init = os.path.join(fastqdir, '{0}{1}_2.fastq.gz'.format(
+                    run, {True:'_pass', False:''}[bool(passonly)]))
+
+            if aspera is not None:
+                # use aspera to download
+
+                # directory name explanation here: 
+                # https://www.ncbi.nlm.nih.gov/books/NBK158899/
+                target = ('anonftp@ftp.ncbi.nlm.nih.gov:'
+                          '/sra/sra-instant/reads/ByRun/sra/SRR/{0}/{1}'
+                          '/{1}.sra'.format(run[ : 6], run))
+
+                asperacmds = [ascp, 
+                        '--overwrite', 
+                            {True:'always', False:'never'}[bool(overwrite)],
+                        '-T',
+                        '-q', # quiet mode
+                        '--policy', 'fair',
+                        '-l500m',
+                        '-L', '.',
+                        '-i', asperakey,
+                        target,
+                        fastqdir]
+                asperaout = subprocess.check_call(asperacmds)
+                downloadedrun = os.path.join(fastqdir, run + '.sra')
+                assert os.path.isfile(downloadedrun)
+
+            else:
+                downloadedrun = None
+
+            # run fastq-dump
+            cmds = [fastq_dump,
+                    '--outdir', fastqdir,
+                    '--gzip',
+                    '--readids',
+                    '--skip-technical',
+                    '--dumpbase',
+                    '--clip',
+                    '--split-files',
+                    ]
+            if passonly:
+                cmds += ['--read-filter', 'pass']
+            if downloadedrun:
+                cmds.append(downloadedrun)
+            else:
+                cmds.append(run)
+            out = subprocess.check_output(cmds)
+
+            os.rename(r1init, r1)
+            os.rename(r2init, r2)
+
+            if downloadedrun:
+                os.remove(downloadedrun)
+                
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
---
+++ tests/test_fastqfromsra.py
@@ -0,0 +1,62 @@
+"""Tests `dms_tools2.sra.fastqFromSRA`.
+
+Requires installation of ``fastq-dump`` and ``ascp``
+for tests to run.
+"""
+
+import os
+import time
+import unittest
+import pandas
+import dms_tools2.sra
+
+
+class test_fastqFromSRA(unittest.TestCase):
+    """Tests `fastqFromSRA` with `aspera=None`.
+    
+    Before running, ensure that `FASTQ-DUMP` is a valid path to a
+    recent version (>= 2.8.2) of the ``fastq-dump`` program."""
+    ASPERA = None
+    FASTQ_DUMP = 'fastq-dump' # test fails if this executable not installed
+
+    def test_fastqFromSRA(self):
+        """Tests `fastqFromSRA`."""
+        fastqdir = os.path.join(
+                os.path.abspath(os.path.dirname(__file__)),
+                './test_fastqfromsra_files/')
+
+        # small file that downloads quickly
+        samples = pandas.DataFrame({'name':['temp'], 'run':['SRR5314806']})
+
+        r1 = os.path.join(fastqdir, 'temp_R1.fastq.gz')
+        r2 = os.path.join(fastqdir, 'temp_R2.fastq.gz')
+        for f in [r1, r2]:
+            if os.path.isfile(f):
+                os.remove(f)
+
+        starttime = time.time()
+        dms_tools2.sra.fastqFromSRA(samples, 
+                self.FASTQ_DUMP,
+                fastqdir,
+                aspera=self.ASPERA)
+        tottime = time.time() - starttime
+        print("Time with{0} aspera: {1}".format(
+                {True:'', False:'out'}[bool(self.ASPERA)], tottime))
+
+        self.assertTrue(os.path.isfile(r1))
+        self.assertTrue(os.path.isfile(r2))
+
+
+class test_fastqFromSRA_aspera(test_fastqFromSRA):
+    """Tests `fastqFromSRA` with `aspera` option.
+
+    Before running, ensure that `ASPERA` gives a valid path
+    to ``ascp`` and aspera key.
+    """
+    ASPERA = ('/app/aspera-connect/3.5.1/bin/ascp',
+              '/app/aspera-connect/3.5.1/etc/asperaweb_id_dsa.openssh')
+
+
+if __name__ == '__main__':
+    runner = unittest.TextTestRunner()
+    unittest.main(testRunner=runner)
