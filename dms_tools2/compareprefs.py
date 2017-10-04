24c728e559e6f5af499cb8dd7c9707264cbce247
Hugh Haddox <haddox@uw.edu>
2017-10-04 09:13:09 -0700

    created module `compareprefs`

---
+++ dms_tools2/compareprefs.py
@@ -0,0 +1,17 @@
+"""
+========================
+compareprefs
+========================
+
+Performs operations related to comparing site-specific amino-acid
+preferences among homologs.
+"""
+
+
+import dms_tools2
+
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
