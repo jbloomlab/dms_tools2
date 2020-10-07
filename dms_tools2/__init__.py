0696baace74fdcbc82dc73e153cbad6ab1b8a899
Vanessa Nguyen <46462831+nguyenvanessa@users.noreply.github.com>
2020-10-07 16:41:15 -0700

    Update path to biopython IUPAC module in __init__

--- dms_tools2/__init__.py
+++ dms_tools2/__init__.py
@@ -52,7 +52,7 @@ AA_TO_CODONS = {
 NT_TO_REGEXP = dict(
         map(lambda tup: (tup[0], tup[1]) if len(tup[1]) == 1 else
                         (tup[0], '[' + ''.join(sorted(tup[1])) + ']'), 
-        Bio.Seq.IUPAC.IUPACData.ambiguous_dna_values.items()))
+        Bio.Data.IUPACData.ambiguous_dna_values.items()))
 
 # import all modules as here: https://stackoverflow.com/a/1057534
 # but do not import `rplot` as it is optional.
